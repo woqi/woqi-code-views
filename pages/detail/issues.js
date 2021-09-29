aba3e735b277ac48fe5b1ee18564b0246e9b30d6
woqi <741644017@qq.com>
2021-09-29 18:03:42 +0800

    10-11

--- pages/detail/issues.js
+++ pages/detail/issues.js
@@ -1,32 +1,127 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback,useEffect } from 'react'
+import { Select, Button, Spin } from 'antd'
 
-import withRepoBasic from "../../components/with-repo-basic";
 const map_api = require('../../lib/map-api')
+import withRepoBasic from '../../components/with-repo-basic'
+import IssueItem from '../../components/IssueItem'
+import SearchUser from '../../components/SearchUser'
 
+const Option = Select.Option
+const isServer = typeof window === 'undefined'
+const CACHE = {}
 
-import IssueItem from "../../components/IssueItem";
-import SearchUser from "../../components/SearchUser";
+function makeQuery(creator, state, labels) {
+  let creatorStr = creator ? `creator=${creator}` : ''
+  let stateStr = state ? `state=${state}` : ''
+  let labelStr = ''
+  if (labels && labels.length > 0) {
+    labelStr = `labels=${labels.join(',')}`
+  }
 
-function Issues({ issues }) {
+  const arr = []
+
+  if (creatorStr) arr.push(creatorStr)
+  if (stateStr) arr.push(stateStr)
+  if (labelStr) arr.push(labelStr)
+
+  return `?${arr.join('&')}`
+}
+function Issues({ issues, labels, owner, name }) {
   const [creator, setCreator] = useState()
+  const [state, setState] = useState()
+  const [label, setLabel] = useState([])
+  const [reprocessingIss, setReprocessingIss] = useState(issues)
+  const [fetching, setFetching] = useState(false)
 
-  const handleCreatorChange = useCallback(value => {
+  useEffect(() => {
+    if (!isServer) {
+      CACHE[`${owner}/${name}`] = labels
+    }
+  }, [owner, name, labels])
+
+  const h_Creator_Change = useCallback(value => {
     setCreator(value)
   }, [])
+  const h_State_Change = useCallback(value => {
+    setState(value)
+  }, [])
+  const h_Label_Change = useCallback(value => {
+    setLabel(value)
+  }, [])
+
+  const h_Search = useCallback(() => {
+    map_api
+      .request({
+        url: `/repos/${owner}/${name}/issues${makeQuery(
+          creator,
+          state,
+          label
+        )}`,
+      })
+      .then(resp => {
+        console.log('h_Search', resp.data)
+        setReprocessingIss(resp.data)
+        setFetching(false)
+      })
+      .catch(err => {
+        console.error(err)
+        setFetching(false)
+      })
+    setFetching(true)
+  }, [owner, name, creator, state, label])
 
   return (
-    <div className="root">
-      <SearchUser />
-      <div className="issues">
+    <div className='root'>
+      <div className='search'>
+        <SearchUser onChange={h_Creator_Change} data={creator} />
 
-        {issues.map(issue => (
-          <IssueItem issue={issue} key={issue.id} />
-        ))}
+        <Select
+          placeholder='状态'
+          onChange={h_State_Change}
+          value={state}
+          style={{ width: 200, marginLeft: 20 }}
+        >
+          <Option value='all'>all</Option>
+          <Option value='open'>open</Option>
+          <Option value='closed'>closed</Option>
+        </Select>
+
+        <Select
+          mode='multiple'
+          placeholder='Lable'
+          onChange={h_Label_Change}
+          value={label}
+          style={{ width: 200, marginLeft: 20 }}
+        >
+          {labels.map(la => {
+            return (
+              <Option value={la.name} key={la.id}>
+                {la.name}
+              </Option>
+            )
+          })}
+        </Select>
+        <Button type='primary'className="search-btn" disabled={fetching} onClick={h_Search}>
+          搜索
+        </Button>
       </div>
 
-      {/* <IssueDetail /> */}
+      {fetching ? (
+        <div className='loading'>
+          <Spin />
+        </div>
+      ) : (
+        <div className='issues'>
+          {reprocessingIss.map(is => (
+            <IssueItem issue={is} key={is.id} />
+          ))}
+        </div>
+      )}
 
       <style jsx>{`
+        .search-btn{
+          margin-left: 20px !important;
+        }
         .issues {
           border: 1px solid #eee;
           border-radius: 5px;
@@ -43,15 +138,34 @@ function Issues({ issues }) {
           justify-content: center;
         }
       `}</style>
-
     </div>
   )
 }
 Issues.getInitialProps = async ({ ctx }) => {
   const { owner, name } = ctx.query
-  const issuesResp = await map_api.request({
-    url: `/repos/${owner}/${name}/issues`
-  }, ctx.req, ctx.res)
-  return { issues: issuesResp.data }
+
+  const res = await Promise.all([
+    await map_api.request(
+      {
+        url: `/repos/${owner}/${name}/issues`,
+      },
+      ctx.req,
+      ctx.res
+    ),
+    await map_api.request(
+      {
+        url: `/repos/${owner}/${name}/labels`,
+      },
+      ctx.req,
+      ctx.res
+    ),
+  ])
+
+  return {
+    owner,
+    name,
+    issues: res[0].data,
+    labels: res[1].data,
+  }
 }
 export default withRepoBasic(Issues, 'issues')
